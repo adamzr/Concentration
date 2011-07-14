6e4dae279dc8245caf7f04e3d47b4a6e2e1b1da5
Adam Richeimer <adamzr@gmail.com>
2011-07-14 16:29:18 -0700

    LinkedIn part 2.

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@ shuffle = function(v){
     for(var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
     return v;
 };
- var GAME = {};
+
+//Global variable
+var GAME = {};
  
 GAME.COLS = 8;
 GAME.ROWS = 4;
@@ -41,7 +43,124 @@ function onLinkedInAuth() {
 }
 
 function createInMatches(connections){
-  console.log(connections);
+  $.each(connections.values, function(index, value){
+    if(value.pictureUrl){
+      GAME.matches.push(new Match(value.firstName + " " + value.lastName, value.pictureUrl));
+    }
+  })
+  localStorage["matches"] = JSON.stringify(GAME.matches);//Save them for next time
+  play();
+}
+
+//Error handler for images
+function usename(event){
+  $(event.srcElement).replaceWith("<p>" + $(event.srcElement).parent(".card").data("name") + "</p>");
+}
+
+// Starts the game
+function play(){
+    //If there's not enough matches we'll need to use the default cards
+    if(GAME.matches.length < GAME.NUMBER_OF_MATCHES){
+        defaultMatches = new Array();
+        defaultMatches.push(new Match("Barack Obama", "http://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Official_portrait_of_Barack_Obama.jpg/100px-Official_portrait_of_Barack_Obama.jpg"));
+        defaultMatches.push(new Match("George W. Bush", "http://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/George-W-Bush.jpeg/100px-George-W-Bush.jpeg"));
+        defaultMatches.push(new Match("Bill Clinton", "http://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Clinton.jpg/100px-Clinton.jpg"));
+        defaultMatches.push(new Match("George H. W. Bush", "http://upload.wikimedia.org/wikipedia/commons/thumb/1/10/George_H._W._Bush_-_portrait_by_Herbert_Abrams_%281994%29.jpg/100px-George_H._W._Bush_-_portrait_by_Herbert_Abrams_%281994%29.jpg"));
+        defaultMatches.push(new Match("Ronald Reagan", "http://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/REAGANWH.jpg/100px-REAGANWH.jpg"));
+        defaultMatches.push(new Match("Jimmy Carter", "http://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/James_E._Carter_-_portrait.gif/99px-James_E._Carter_-_portrait.gif"));
+        defaultMatches.push(new Match("Gerald Ford", "http://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Gerald_R._Ford_-_portrait.jpg/100px-Gerald_R._Ford_-_portrait.jpg"));
+        defaultMatches.push(new Match("Richard Nixon", "http://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Richard_Nixon_-_Presidential_portrait.jpg/100px-Richard_Nixon_-_Presidential_portrait.jpg"));
+        defaultMatches.push(new Match("Lyndon B. Johnson", "http://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Lyndon_B._Johnson_-_portrait.png/100px-Lyndon_B._Johnson_-_portrait.png"));
+        defaultMatches.push(new Match("John F. Kennedy", "http://upload.wikimedia.org/wikipedia/commons/thumb/2/21/John_F_Kennedy_Official_Portrait.jpg/100px-John_F_Kennedy_Official_Portrait.jpg"));
+        defaultMatches.push(new Match("Dwight D. Eisenhower", "http://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Dwight_D._Eisenhower%2C_official_Presidential_portrait.jpg/100px-Dwight_D._Eisenhower%2C_official_Presidential_portrait.jpg"));
+        defaultMatches.push(new Match("Harry S. Truman", "http://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/HarryTruman.jpg/100px-HarryTruman.jpg"));
+        defaultMatches.push(new Match("Franklin D. Roosevelt", "http://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Franklin_Roosevelt_-_Presidential_portrait.jpg/100px-Franklin_Roosevelt_-_Presidential_portrait.jpg"));
+        defaultMatches.push(new Match("Herbert Hoover", "http://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Herbert_Clark_Hoover_by_Greene%2C_1956.jpg/100px-Herbert_Clark_Hoover_by_Greene%2C_1956.jpg"));
+        defaultMatches.push(new Match("Calvin Coolidge", "http://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Calvin_Coolidge.jpg/100px-Calvin_Coolidge.jpg"));
+        defaultMatches.push(new Match("Warren G. Harding", "http://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Wh29.gif/100px-Wh29.gif"));
+        //We should use only as many default cards as needed
+        while(GAME.matches.length < GAME.NUMBER_OF_MATCHES){
+            GAME.matches.push(defaultMatches.shift());
+        }
+    }
+    
+    //Shuffle the matches
+    GAME.matches = shuffle(GAME.matches);
+    
+    var cards = new Array(GAME.CARDS);
+    
+    for(i = 0; i < GAME.CARDS; i++){
+        cards[i] = i;
+    }
+    
+    cards = shuffle(cards);
+    
+    //Assign matches to cards
+    for(i = 0, j = 0; i < GAME.NUMBER_OF_MATCHES; i++, j += 2){
+        var match = GAME.matches[i];
+        var $card1 = $(".card").eq(cards[j]);
+        var $card2 = $(".card").eq(cards[j + 1]);
+        //If the image source is a valid URL use the face image, otherwise use just the name
+        if(match.imgSrc.trim().length > 0){
+          $card1.find(".back").append("<span></span><img class='face' src='" + match.imgSrc + "' id='face" + i +"' onerror='usename'></img>");
+        }
+        else{
+          $card1.find(".back").append("<p>" + match.name + "</p>");
+        }
+        
+        $card2.find(".back").append("<p>" + match.name + "</p>");
+        $card1.data("name", match.name);
+        $card2.data("name", match.name);
+    }
+    
+    function timer(){
+      $("#seconds").text(parseInt($("#seconds").text()) + 1);
+    }
+    
+    //If the image doesn't load use the name instead of the face
+    $(".face").live("error", function(){
+      $(this).replaceWith("<p>" + $(this).parent(".card").data("name") + "</p>");
+     });
+    
+    //Handle clicks on cards
+    $(".card").click(function(){
+        if(!GAME.started){
+            GAME.started = true;
+            GAME.timerInteval = setInterval(timer, 1000);
+        }
+        if( $(this).hasClass("flipped")){
+            //If you turn over one you already turned over, all get turned back over
+            $(".flipped").removeClass("flipped");
+        }
+        else{
+            var flippedOver = $(".flipped").length;
+            //If there's 2 flipped cards
+            if(flippedOver > 1){
+                $(".flipped").removeClass("flipped");
+            }
+            $(this).toggleClass("flipped");
+            
+            if(flippedOver === 1){
+                if($(".flipped").eq(0).data("name") === $(".flipped").eq(1).data("name")){
+                    $(".flipped").fadeOut(3000,
+                    function(){
+                      $(this).remove();
+                      if($(".card").length === 0){
+                        clearInterval(GAME.timerInteval);
+                        var seconds = Math.max(parseInt($("#seconds").text()) - 3, 0);
+                        $("#seconds").text( seconds );
+                        $("#main").html("<div id='winmessage'><h1>You Won in " + seconds + " seconds!</h1><br /><a href='javascript:location.reload()'>Play again?</a></div>");
+                        $("#winmessage").center();
+                      }
+                    }
+                    );
+                }
+            }
+        }
+        
+    });
+    
+    $.unblockUI();
 }
 
 //When the document is ready start the game
@@ -161,109 +280,5 @@ $(document).ready(function(){
         play();
     }
     
-    //Start the game!
-    function play(){
-        //If there's not enough matches we'll need to use the default cards
-        if(GAME.matches.length < GAME.NUMBER_OF_MATCHES){
-            defaultMatches = new Array();
-            defaultMatches.push(new Match("Barack Obama", "http://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Official_portrait_of_Barack_Obama.jpg/100px-Official_portrait_of_Barack_Obama.jpg"));
-            defaultMatches.push(new Match("George W. Bush", "http://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/George-W-Bush.jpeg/100px-George-W-Bush.jpeg"));
-            defaultMatches.push(new Match("Bill Clinton", "http://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Clinton.jpg/100px-Clinton.jpg"));
-            defaultMatches.push(new Match("George H. W. Bush", "http://upload.wikimedia.org/wikipedia/commons/thumb/1/10/George_H._W._Bush_-_portrait_by_Herbert_Abrams_%281994%29.jpg/100px-George_H._W._Bush_-_portrait_by_Herbert_Abrams_%281994%29.jpg"));
-            defaultMatches.push(new Match("Ronald Reagan", "http://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/REAGANWH.jpg/100px-REAGANWH.jpg"));
-            defaultMatches.push(new Match("Jimmy Carter", "http://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/James_E._Carter_-_portrait.gif/99px-James_E._Carter_-_portrait.gif"));
-            defaultMatches.push(new Match("Gerald Ford", "http://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Gerald_R._Ford_-_portrait.jpg/100px-Gerald_R._Ford_-_portrait.jpg"));
-            defaultMatches.push(new Match("Richard Nixon", "http://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Richard_Nixon_-_Presidential_portrait.jpg/100px-Richard_Nixon_-_Presidential_portrait.jpg"));
-            defaultMatches.push(new Match("Lyndon B. Johnson", "http://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Lyndon_B._Johnson_-_portrait.png/100px-Lyndon_B._Johnson_-_portrait.png"));
-            defaultMatches.push(new Match("John F. Kennedy", "http://upload.wikimedia.org/wikipedia/commons/thumb/2/21/John_F_Kennedy_Official_Portrait.jpg/100px-John_F_Kennedy_Official_Portrait.jpg"));
-            defaultMatches.push(new Match("Dwight D. Eisenhower", "http://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Dwight_D._Eisenhower%2C_official_Presidential_portrait.jpg/100px-Dwight_D._Eisenhower%2C_official_Presidential_portrait.jpg"));
-            defaultMatches.push(new Match("Harry S. Truman", "http://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/HarryTruman.jpg/100px-HarryTruman.jpg"));
-            defaultMatches.push(new Match("Franklin D. Roosevelt", "http://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Franklin_Roosevelt_-_Presidential_portrait.jpg/100px-Franklin_Roosevelt_-_Presidential_portrait.jpg"));
-            defaultMatches.push(new Match("Herbert Hoover", "http://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Herbert_Clark_Hoover_by_Greene%2C_1956.jpg/100px-Herbert_Clark_Hoover_by_Greene%2C_1956.jpg"));
-            defaultMatches.push(new Match("Calvin Coolidge", "http://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Calvin_Coolidge.jpg/100px-Calvin_Coolidge.jpg"));
-            defaultMatches.push(new Match("Warren G. Harding", "http://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Wh29.gif/100px-Wh29.gif"));
-            //We should use only as many default cards as needed
-            while(GAME.matches.length < GAME.NUMBER_OF_MATCHES){
-                GAME.matches.push(defaultMatches.shift());
-            }
-        }
-        
-        //Shuffle the matches
-        GAME.matches = shuffle(GAME.matches);
-        
-        cards = new Array(GAME.CARDS);
-        
-        for(i = 0; i < GAME.CARDS; i++){
-            cards[i] = i;
-        }
-        
-        cards = shuffle(cards);
-        
-        //Assign matches to cards
-        for(i = 0, j = 0; i < GAME.NUMBER_OF_MATCHES; i++, j += 2){
-            var match = GAME.matches[i];
-            var $card1 = $(".card").eq(cards[j]);
-            var $card2 = $(".card").eq(cards[j + 1]);
-            //If the image source is a valid URL use the face image, otherwise use just the name
-            if(match.imgSrc.trim().length > 0){
-              $card1.find(".back").append("<span></span><img class='face' src='" + match.imgSrc + "' id='face" + i +"'></img>");
-            }
-            else{
-              $card1.find(".back").append("<p>" + match.name + "</p>");
-            }
-            
-            $card2.find(".back").append("<p>" + match.name + "</p>");
-            $card1.data("name", match.name);
-            $card2.data("name", match.name);
-        }
-        
-        function timer(){
-          $("#seconds").text(parseInt($("#seconds").text()) + 1);
-        }
-        
-        //If the image doesn't load use the name instead of the face
-        $(".face").live("error", function(){
-          $(this).replaceWith("<p>" + $(this).parent(".card").data("name") + "</p>");
-         });
-        
-        //Handle clicks on cards
-        $(".card").click(function(){
-            if(!GAME.started){
-                GAME.started = true;
-                GAME.timerInteval = setInterval(timer, 1000);
-            }
-            if( $(this).hasClass("flipped")){
-                //If you turn over one you already turned over, all get turned back over
-                $(".flipped").removeClass("flipped");
-            }
-            else{
-                var flippedOver = $(".flipped").length;
-                //If there's 2 flipped cards
-                if(flippedOver > 1){
-                    $(".flipped").removeClass("flipped");
-                }
-                $(this).toggleClass("flipped");
-                
-                if(flippedOver === 1){
-                    if($(".flipped").eq(0).data("name") === $(".flipped").eq(1).data("name")){
-                        $(".flipped").fadeOut(3000,
-                        function(){
-                          $(this).remove();
-                          if($(".card").length === 0){
-                            clearInterval(GAME.timerInteval);
-                            var seconds = Math.max(parseInt($("#seconds").text()) - 3, 0);
-                            $("#seconds").text( seconds );
-                            $("#main").html("<div id='winmessage'><h1>You Won in " + seconds + " seconds!</h1><br /><a href='javascript:location.reload()'>Play again?</a></div>");
-                            $("#winmessage").center();
-                          }
-                        }
-                        );
-                    }
-                }
-            }
-            
-        });
-        
-        $.unblockUI();
-    }
+
 });
